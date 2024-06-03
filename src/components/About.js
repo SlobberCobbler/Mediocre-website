764fc1a6456ba8daa003f62903601fa6ce5ce700
SlobberCobbler <65059918+SlobberCobbler@users.noreply.github.com>
2024-06-03 01:28:24 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,14 +12,14 @@ const About = () => {
             <div>
               <h4 className="h-color">⚡ Commands</h4>
               <p className="p-color">
-                We have more than 200 commands support in this bot.
+                We have more than 500 commands support in this bot.
               </p>
               <hr />
             </div>
             <div>
               <h4 className="h-color">✉️ 24/7 Support</h4>
               <p className="p-color">
-                The biggest support community server for the bot available for
+                Chill and laid back support community server for the bot available for
                 you anytime.
               </p>
               <hr />
@@ -27,7 +27,7 @@ const About = () => {
             <div>
               <h4 className="h-color">✏️ Customization</h4>
               <p className="p-color">
-                Fully customizable discord bot completely FREE!
+                Fully customizable discord bot to your liking!
               </p>
               <hr />
             </div>
@@ -41,15 +41,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
+            <h4 className="h-color">93</h4>
             <p className="p-color">Total Servers</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
+            <h4 className="h-color">154k+</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
+            <h4 className="h-color">512</h4>
             <p className="p-color">Total Commands</p>
           </div>
         </section>
